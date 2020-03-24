bab79c85e86a8feeebc821ec5270138206f20bbe
Saksham Mittal <gotlougit@users.noreply.github.com>
2020-03-24 19:26:54 +0530

    Added compareLog() function

--- gotlougit/main.py
+++ gotlougit/main.py
@@ -4,6 +4,8 @@ def removeTimeStamp(log):
     #f = sys.stdin.read().split('\n')
     #f.remove('')
     f = log.split('\n')
+    f.remove('')
+
     out = ""
 
     for i in f:
@@ -11,10 +13,20 @@ def removeTimeStamp(log):
         x.pop(0)
         x.pop(0)
         x.pop(0)
-    
+        x.pop(0)
+
         y = ""
         for j in x:
             y += j + ' '
         out += y + '\n'
-    return out.splitlines()
+    return out
+
+def compareLog(log1,log2):
+    repeats = []
+    for line in log1:
+        if line in log2 and line not in repeats:
+            repeats.append(line)
+            print(line)
+    return repeats
+
 
