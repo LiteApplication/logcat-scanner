9acca4d66a4ae789ad231193ea555765af36c738
LiteApplication <contact.liteapplication@gmail.com>
2020-03-24 15:03:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from gotlougit import main
 version = 0.2
 LOG_FILE_1 = "logcat_1.log.txt"
 LOG_FILE_2 = "logcat_2.log.txt"
+LOG_FILE_COMMON = "logcat_c.log.txt"
 ###########################
 
 print(f"Welcome to Logcat-Scanner v{version}\n by Gotlou and LiteApplication")
@@ -19,5 +20,10 @@ log2_file.close()
 
 log1 = main.removeTimeStamp(log1)
 log2 = main.removeTimeStamp(log2)
+
+common = main.compareLog(log1, log2)
+with open(LOG_FILE_COMMON, "r") as f:
+    f.writelines(common)
+print("Done.")
 
 
