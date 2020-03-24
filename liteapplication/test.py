d89393a0bf2dd766826d4a642bdb555b32610caf
LiteApplication <contact.liteapplication@gmail.com>
2020-03-24 14:36:42 +0100

    Rename test.py

--- liteapplication/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-###########################
-LOG_FILE_1 = "logcat_1.log.txt"
-LOG_FILE_2 = "logcat_2.log.txt"
-###########################
-
---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from gotlougit import main
+
+###########################
+version = 0.2
+LOG_FILE_1 = "logcat_1.log.txt"
+LOG_FILE_2 = "logcat_2.log.txt"
+###########################
+
+print(f"Welcome to Logcat-Scanner v{version}\n by Gotlou and LiteApplication")
+log1_file = open(LOG_FILE_1, 'r')
+log1 = log1_file.read()
+log1_file.close()
+
+log2_file = open(LOG_FILE_2, 'r')
+log2 = log2_file.read()
+log2_file.close()
+
+log1 = main.removeTimeStamp(log1)
+log2 = main.removeTimeStamp(log2)
+
+
